867c8619f4bf27dec698d4289840aec8f20682a0
JPABLOMACAYA <103909266+JPABLOMACAYA@users.noreply.github.com>
2022-05-26 17:30:15 -0400

    Add files via upload

---
+++ asincrono3.js
@@ -0,0 +1,15 @@
+
+let inputPrincipal = document.querySelector("#input1");
+
+let botonAgregar = document.querySelector(".boton-agregar");
+
+let contenedor = document.querySelector(".container");
+
+class Item {
+    constructor(nuevaTarea) {
+        this.crearDiv(nuevaTarea)
+    }
+    crearDiv(nuevaTarea) {
+
+    }
+}
